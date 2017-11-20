190ce08ae620f2b52af0e394d268ef30835400a2
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-11-20 04:14:21 -0500

    Added a pause function incase you would want to pause again some where else in your script there woul dbe no need to rewrite the code to do so

--- github-push.sh
+++ github-push.sh
@@ -7,7 +7,9 @@
 # An example of what it should look like when you run your script from the terminal
 # $ ./github-push.sh 'Add your comment here' origin master
 
-
+#The -n defines the required character count to stop reading and -s hides the user's input, add "minus r" to the read flags, which will cause the string to be interpreted as raw without considering any backslash escapes
+#read -n 1 -s -r -p "If your commit looks right, Press any key to continue"
+# or you can use the following function so if you need to pause mutiple times in a script there is no need to rewrite it time and again, the -r -s -p can be added together if you wish as well as -n and 1 
 pause(){
  read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
 }
@@ -20,8 +22,7 @@ git add .
 git status
 
 # Asks user to confirm that the commit looks right and if it does push any key to push it to the branch that the suppiled in the command line when running the script
-#The -n defines the required character count to stop reading and -s hides the user's input, add "minus r" to the read flags, which will cause the string to be interpreted as raw without considering any backslash escapes
-#read -n 1 -s -r -p "If your commit looks right, Press any key to continue"
+
 
 pause
 #Commits the files to be pushed to Github
