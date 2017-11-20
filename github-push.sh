3ef584a59b1a8669bed2696c2738704cdc10c7a3
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-11-20 03:09:16 -0500

    Added cooments to explain every step of the script so far

--- github-push.sh
+++ github-push.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Adds the recently changed files to your commit
 git add .
+# Checks with user to make sure commit looks right
 git status
+# Asks user to confirm that the commit looks right and if it does push any key to push it to the branch that the suppiled in the command line when running the script
 read -n 1 -s -r -p "If your commit looks right, Press any key to continue"
+#Commits the files to be pushed to Github
 git commit -m "$1"
+#Pushes the files in your commit to github
 git push $2 $3
