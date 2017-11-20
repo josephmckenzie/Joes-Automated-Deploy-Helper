c75706e4bf17fe13562c90656caf127414415d75
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-11-20 05:31:51 -0500

    rewording prompts to user to mkae more helpful

--- github-push.sh
+++ github-push.sh
@@ -3,9 +3,9 @@
 #
 ## $ ./github-push.sh 'First Argument(Your commit message Put within quotes so you can put spaces ir else it will think every word seperated by spaces is a new agrument)' Second-Agrument(Ie: origin) Third-Agrument is your branch(Ie: master)
 #
-## An example of what it should look like when you run your script from the terminal
+## **** An example of what it should look like when you run your script from the terminal ****
 ## $ ./github-push.sh 'Add your comment here' origin master
-#
+# Remember any branch can be used still it doesnt have to be master
 ##The -n defines the required character count to stop reading and -s hides the user's input, add "minus r" to the read flags, which will cause the string to be interpreted as raw without considering any backslash escapes
 ##read -n 1 -s -r -p "If your commit looks right, Press any key to continue"
 ## or you can use the following function so if you need to pause mutiple times in a script there is no need to rewrite it time and again, the -r -s -p can be added together if you wish as well as -n and 1 
@@ -14,6 +14,8 @@
 # read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
 #}
 #
+#### Old script using an if else statement doesnot loop until users says yes it looks right
+
 ## Adds the recently changed files to your commit
 #git add .
 ##
@@ -37,30 +39,29 @@
 #        read Useranswer
 #        
 #    fi
-## Asks user to confirm that the commit looks right and if it does push any key to push it to the branch that the suppiled in the command line when running the script
-
+####
 
 #!/bin/env bash
+# adds all the changed files to the commit
 git add .
-#
-## Displays the files you are commiting and Checks with user to make sure commit looks right.
+## Displays the files you are commiting to show user the commit and make sure it looks right and all files should be committed.
 git status
-#
+#asks the user if the commit looks right and until Y or y is entered it will not push the commit but as you to enter the file you wish to remove from the commit
 echo -n "Does your commit look right? (y/n)"
-read ans
-while [ $ans == n ] || [ $ans == N ] ;
+#read is the same thing basically as gets in ruby Ie: ask for user input and stores the answer to be used in the variable name 
+read Useranswer
+while [ $Useranswer == n ] || [ $Useranswer == N ] ;
 do
-  echo -n 'Please enter the name of the file you wish to remove: '
+  echo -n 'Please enter the name of the file you wish to remove or press CTRL+C to cancel :'
   read file
   git reset HEAD $file
 		git status
   echo -n "Does your commit look right? (y/n)"
-  read ans
+  read Useranswer
 done
-
+#Having said yes the commit looks correct it will push
 #Commits the files to be pushed to Github
-       git commit -m "$1"
-#       
-#							#Pushes the files in your commit to github
-       git push $2 $3 
+git commit -m "$1"    
+#Pushes the files in your commit to github to the origin/branch specified whe launching the script through 
+git push $2 $3 
 
