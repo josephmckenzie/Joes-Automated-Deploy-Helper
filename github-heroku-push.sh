c03d441b43a6b83eacc054bb50b421766012c778
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-11-20 04:11:29 -0500

    Added a pause function incase you would want to pause again some where else in your script there woul dbe no need to rewrite the code to do so

--- github-heroku-push.sh
+++ github-heroku-push.sh
@@ -5,8 +5,12 @@
 
 
 # An example of what it should look like when you run your script from the terminal
-
 # $ ./github-push.sh 'Add your comment here' origin master
+
+
+pause(){
+ read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
+}
 
 
 # Adds the recently changed files to your commit
@@ -16,9 +20,10 @@ git add .
 git status
 
 # Asks user to confirm that the commit looks right and if it does push any key to push it to the branch that the suppiled in the command line when running the script
-#The -n 1 is the required characters read, the -s hides user input
-read -n 1 -s -r "If your commit looks right, Press any key to continue"
+#The -n defines the required character count to stop reading and -s hides the user's input, add "minus r" to the read flags, which will cause the string to be interpreted as raw without considering any backslash escapes
+#read -n 1 -s -r -p "If your commit looks right, Press any key to continue"
 
+pause
 #Commits the files to be pushed to Github
 git commit -m "$1"
 #Pushes the files in your commit to github
--- github-push.sh
+++ github-push.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
+#This script will push to GitHub and Heroku saving you time from having to manually push to both or to actually have to go to heroku to push you changed code each time.
+
+# $ ./github-push.sh 'First Argument(Your commit message Put within quotes so you can put spaces ir else it will think every word seperated by spaces is a new agrument)' Second-Agrument(Ie: origin) Third-Agrument is your branch(Ie: Master)
+
+
+# An example of what it should look like when you run your script from the terminal
+# $ ./github-push.sh 'Add your comment here' origin master
+
+
+pause(){
+ read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
+}
+
 
 # Adds the recently changed files to your commit
 git add .
-# Checks with user to make sure commit looks right
+
+# Displays the files you are commiting and Checks with user to make sure commit looks right.
 git status
+
 # Asks user to confirm that the commit looks right and if it does push any key to push it to the branch that the suppiled in the command line when running the script
-read -n 1 -s -r -p "If your commit looks right, Press any key to continue"
+#The -n defines the required character count to stop reading and -s hides the user's input, add "minus r" to the read flags, which will cause the string to be interpreted as raw without considering any backslash escapes
+#read -n 1 -s -r -p "If your commit looks right, Press any key to continue"
+
+pause
 #Commits the files to be pushed to Github
 git commit -m "$1"
 #Pushes the files in your commit to github
