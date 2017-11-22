6d4878fe4d2599bdc1f6764c911ecb8190fefa70
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-11-22 05:03:55 -0500

    github create new repo from bash script and push readme working correct from script file

--- testing.sh
+++ testing.sh
@@ -1,4 +1,4 @@
-##!/bin/env bash
+##!/bin/env bash -x
 add_check_commit(){
 git add .
 				git status
@@ -32,17 +32,29 @@ git add .
 		read MainService 
 		if [ "$MainService" = 1 ]; then
 				echo "What would you like to do today?"
-#				echo -e "3. Create new Repo"
-				echo -e "1. Pull"
-				echo -e "2. Push"
+				echo -e "1. Create new Repo"
+				echo -e "2. Pull"
+				echo -e "3. Push"
 				echo -n "Enter a number: "
 				read Github
-		if [ "$Github" = 1 ]; then
+		if [ "$Github" = 2 ]; then
 				git pull origin master
-		elif [ "$Github" = 2 ]; then
-				add_check_commit
 		elif [ "$Github" = 3 ]; then
-			echo "Please enter a new repo name: "
+				add_check_commit
+		elif [ "$Github" = 1 ]; then
+			 echo "Please enter your profile name: "
+			 read profilename
+			 echo "Please enter a new repo name: "
+			 read RepoName
+			 curl --user $profilename https://api.github.com/user/repos -d "{\"name\":\"$RepoName\"}"
+    echo "$RepoName" >> README.md
+    git init
+    git add README.md
+				echo "Please enter a commit message: "
+				read createrepocommitmessage
+    git commit -m "$createrepocommitmessage"
+    git remote add origin https://github.com/"$profilename"/"$RepoName".git
+    git push -u origin master
 		
 		fi
 		elif [ "$MainService" = 2 ]; then
@@ -81,4 +93,6 @@ git add .
       echo "Amazon's Codedeploy"        
     fi						
  fi
+	
+	
 				
