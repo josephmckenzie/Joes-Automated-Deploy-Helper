694903f26a6d2d57818d308842c360037f00449e
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-11-23 23:09:52 -0500

    meny reset tes

--- all-services.sh
+++ all-services.sh
@@ -115,7 +115,7 @@ do
         "Push")
 								    Github_add_check_commit
             echo "Pushing Code"
-											echo $opt
+												echo $options
             ;;
         "Quit")
             break
