dd68ec071536c5f8f554a856712be3da4a214cd5
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-11-24 00:37:06 -0500

    added create IAM user to amazon menu

--- all-services.sh
+++ all-services.sh
@@ -89,8 +89,17 @@ if [ "$1" == -h ]; then
 	exit 0
 	fi
 
-GithubOptionMenu() {
- GithubOptions=("Create New Repo" "Pull" "Push" "Quit")
+
+
+#PS3 is a default prompt statment for bash PS3 is for 
+PS3='Please enter your choice: '
+options=("Github" "Heroku" "Amazon" "Help" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Github")
+           
+										    GithubOptions=("Create New Repo" "Pull" "Push" "Quit")
 select GithubOpt in "${GithubOptions[@]}"
 do
     case $GithubOpt in
@@ -114,18 +123,8 @@ do
             ;;
         *) echo invalid option;;
     esac
-				GithubOptionMenu
+				
 done
-}
-
-#PS3 is a default prompt statment for bash PS3 is for 
-PS3='Please enter your choice: '
-options=("Github" "Heroku" "Amazon (Work in Progress)" "Help" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Github")
-           GithubOptionMenu
 												break
             ;;
         "Heroku")
@@ -163,8 +162,33 @@ select AmazonOpt in "${AmazonOptions[@]}"
 do
     case $AmazonOpt in
         "IAM Config")
-            echo "You chose IAM config"
+ AmazonOptions=("New IAM user" "New IAM role" "IAM policies" "Quit")
+select AmazonOpt in "${AmazonOptions[@]}"
+do
+    case $AmazonOpt in
+        "New IAM user")
+								    echo "Please enter a name for your new IAM user"
+												read IAMUSER
+            aws iam create-user --user-name "$IAMUSER"
+												echo "Creating new IAM user"
 												break
+            ;;
+        "New IAM role")
+            echo "Create new IAM role"
+												break
+            ;;
+        "IAM policies")
+            echo "you chose IAM policies"
+												break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+				
+done												
+break
             ;;
         "Lambda")
             echo "You chose Lambda"
@@ -194,104 +218,3 @@ done
     esac
 				
 done
-
-#	openingmessage() {
-#	echo  "Where would you like to push/commit code to today? :"
-#	echo -e "1. Github"
-#	echo -e "2. Heroku"
-#	echo -e "3. Amazon"
-#	echo -n "Enter a number: "
-#	}
-#	   doWithAmazon() {
-#	    echo "What would you like to do today?"
-#				 echo -e "1. Add new IAM user"
-#				 echo -e "2. Add new IAM role"
-#				 echo -e "3. Set premissions and policies"
-#					echo -n "Please enter a number: "
-#	}
-#	
-#	goWithGitHub() {
-#   	echo "What would you like to do today?"
-#				echo -e "1. Create new Repo"
-#				echo -e "2. Pull"
-#				echo -e "3. Push"
-#				echo -n "Enter a number: "
-#				read Github
-#
-#	}
-#	
-#	 openingmessage
-#		read MainService 
-#		if [ "$MainService" = 1 ]; then
-#				goWithGitHub
-#		  if [ "$Github" = 1 ]; then				
-#						echo "Please enter your profile name: "
-#			   read profilename
-#			   echo "Please enter a new repo name: "
-#			   read RepoName
-#			   curl --user $profilename https://api.github.com/user/repos -d "{\"name\":\"$RepoName\"}"
-#      echo "$RepoName" >> README.md
-#      git init
-#      git add README.md
-#				  echo "Please enter a commit message: "
-#				  read createrepocommitmessage
-#      git commit -m "$createrepocommitmessage"
-#      git remote add origin https://github.com/"$profilename"/"$RepoName".git
-#      git push -u origin master
-#		  elif [ "$Github" = 2 ]; then
-#						git pull origin master
-#		  elif [ "$Github" = 3 ]; then
-#				  add_check_commit
-#		  fi
-#		elif [ "$MainService" = 2 ]; then
-#	   	echo "What would you like to do today?"
-#	   	echo -e "1. Create new App"
-#					echo -e "2. Push"
-#					echo -e "3. Add Config Variables"
-#					echo -n "Enter a number: "
-#					read Heroku
-#		if [ "$Heroku" = 3 ]; then
-#		  echo "Please add a Config Variables key: "
-#					read configkey
-#	   echo "Please enter a Config Variables value: "
-#					read envvconfigvalue
-#    	heroku config:set $configkey=$envvconfigvalue	
-#				elif [ "$Heroku" = 2 ]; then
-#	   	add_check_commit
-#				elif [ "$Heroku" = 1 ]; then
-#					echo "Please enter a new for your app: "
-#					read HerokuAppName
-#					heroku create $HerokuAppName
-#		fi
-#		elif [ "$MainService" = 3 ]; then
-#					echo  "What service would you like to update? :"
-#					echo -e "1. Manage IAM users, roles, policies"
-#					echo -e "2. Amazon's Lambda"
-#					echo -e "3. Amazon's S3 Bucket"
-#					echo -e "4. Amazon's Codedeploy"
-#					echo -n "Enter a number: "
-#    read AmazonService 
-#				if [ "$AmazonService" = 1 ]; then
-#				doWithAmazon
-#				read AWS_IAM
-#				if [ "$AWS_IAM" = 1 ]; then
-#				 echo "IAM user"
-#				elif [ "$AWS_IAM" = 2 ]; then
-#				 echo "IAM ROLE"
-#			 elif [ "$AWS_IAM" = 3 ]; then
-#				 echo "Premissions and Policies"
-#				fi
-#    elif [ "$AmazonService" = 2 ]; then
-#      echo "Amazon's Lambda"
-#    elif [ "$AmazonService" = 3 ]; then
-#      echo "Amazon's S3 Bucket"
-#				elif [ "$AmazonService" = 4 ]; then
-#      echo "Amazon's Codedeploy"   
-#				else
-#					 echo -e "\n*** Please pick from the menu ***"
-#					 doWithAmazon
-#    fi				
-#
-# fi	
-#	
-#				
